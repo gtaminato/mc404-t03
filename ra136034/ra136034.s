deedd49cd8d8a8ea7976041fe6e267679b1a8d95
Hugo Kiyodi Oshiro <ra146446@gomes.lab.ic.unicamp.br>
2014-06-21 18:21:46 -0300

    changes

--- ra136034/ra136034.s
+++ ra136034/ra136034.s
@@ -7,6 +7,8 @@ interrupt_vector:
     b RESET_HANDLER
 .org 0x08
     b SUPERVISOR_HANDLER
+.org 0x18
+    b SCHEDULE_HANDLE
 .org 0x100
 .text
 
@@ -78,8 +80,49 @@ RESET_HANDLER:
         ldr r1, =GPT_BASE
         str r0, [r1, #GPT_IR]            @show interest in Output Compare Channel 1
         
-        msr  CPSR_c,  #0x13   @ SUPERVISOR mode, IRQ/FIQ enabled
-    
+    @Set GPT as made in lab07
+    SET_TZIC:
+        @ Constantes para os enderecos do TZIC
+        .set TZIC_BASE,                0x0FFFC000
+        .set TZIC_INTCTRL,             0x0
+        .set TZIC_INTSEC1,             0x84 
+        .set TZIC_ENSET1,              0x104
+        .set TZIC_PRIOMASK,            0xC
+        .set TZIC_PRIORITY9,           0x424
+
+        @ Liga o controlador de interrupcoes
+        @ R1 <= TZIC_BASE
+
+        ldr    r1, =TZIC_BASE
+
+        @ Configura interrupcao 39 do GPT como nao segura
+        mov    r0, #(1 << 7)
+        str    r0, [r1, #TZIC_INTSEC1]
+
+        @ Habilita interrupcao 39 (GPT)
+        @ reg1 bit 7 (gpt)
+
+        mov    r0, #(1 << 7)
+        str    r0, [r1, #TZIC_ENSET1]
+
+        @ Configure interrupt39 priority as 1
+        @ reg9, byte 3
+
+        ldr r0, [r1, #TZIC_PRIORITY9]
+        bic r0, r0, #0xFF000000
+        mov r2, #1
+        orr r0, r0, r2, lsl #24
+        str r0, [r1, #TZIC_PRIORITY9]
+
+        @ Configure PRIOMASK as 0
+        eor r0, r0, r0
+        str r0, [r1, #TZIC_PRIOMASK]
+
+        @ Habilita o controlador de interrupcoes
+        mov    r0, #1
+        str    r0, [r1, #TZIC_INTCTRL]
+              
+    SET_STACKS:
         .set SVC_STACK, 0x77701000
         .set UND_STACK, 0x77702000
         .set ABT_STACK, 0x77703000
@@ -87,18 +130,18 @@ RESET_HANDLER:
         .set FIQ_STACK, 0x77705000
         .set USR_STACK, 0x77706000
 
-    @Configure stacks for all modes
-    mov sp, #SVC_STACK
-    msr CPSR_c, #0xDF  @ Enter system mode, FIQ/IRQ disabled
-    mov sp, #USR_STACK
-    msr CPSR_c, #0xD1  @ Enter FIQ mode, FIQ/IRQ disabled
-    mov sp, #FIQ_STACK
-    msr CPSR_c, #0xD2  @ Enter IRQ mode, FIQ/IRQ disabled
-    mov sp, #IRQ_STACK
-    msr CPSR_c, #0xD7  @ Enter abort mode, FIQ/IRQ disabled
-    mov sp, #ABT_STACK
-    msr CPSR_c, #0xDB  @ Enter undefined mode, FIQ/IRQ disabled
-    mov sp, #UND_STACK
+        @Configure stacks for all modes
+        mov sp, #SVC_STACK
+        msr CPSR_c, #0xDF  @ Enter system mode, FIQ/IRQ disabled
+        mov sp, #USR_STACK
+        msr CPSR_c, #0xD1  @ Enter FIQ mode, FIQ/IRQ disabled
+        mov sp, #FIQ_STACK
+        msr CPSR_c, #0xD2  @ Enter IRQ mode, FIQ/IRQ disabled
+        mov sp, #IRQ_STACK
+        msr CPSR_c, #0xD7  @ Enter abort mode, FIQ/IRQ disabled
+        mov sp, #ABT_STACK
+        msr CPSR_c, #0xDB  @ Enter undefined mode, FIQ/IRQ disabled
+        mov sp, #UND_STACK
 
     @ Initialize processes
     ldr r0, =array_process
@@ -285,6 +328,9 @@ SUPERVISOR_HANDLER:
 
     SUPERVISOR_HANDLER_EXIT:
         movs pc, lr
+        
+SCHEDULE_HANDLE:
+    
 
 main:
     ldr r0, =current_process
@@ -406,4 +452,4 @@ contexts: .space 512
 .org 0x13000
 return_array:       .space 32
 current_process:    .space 4
-array_process:      .space 8
+array_process:      .space 8    @array of process containing one byte for each process if 0->dead, if 1->alive
